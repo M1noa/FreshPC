bf35afc73fd2af75095d5109b9c80d7f0ff5e4c7
Minoa <57844837+M1noa@users.noreply.github.com>
2024-11-04 11:08:04 -0600

    Update Fresh PC.bat

--- Fresh PC.bat
+++ Fresh PC.bat
@@ -133,7 +133,7 @@ DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /ScanHealth
 DISM /Online /Cleanup-Image /RestoreHealth
 sfc /scannow
-chkdsk /f /r /b 
+echo Y | chkdsk /f /r /b
 for /f "tokens=3*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkCards" /f "ServiceName" /s^|findstr /i /l "ServiceName"') do (
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v "TCPNoDelay" /d "1" /t REG_DWORD /f
 reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%i" /v "TcpAckFrequency" /d "1" /t REG_DWORD /f
